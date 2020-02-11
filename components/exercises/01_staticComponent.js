67fe165dfa4dcd0c2b8c2f27013c435204b03727
Bartosz Osowski <bartosz.osowski@errigal.com>
2020-02-11 22:58:58 +0000

    Exercise 1 completed.

--- components/exercises/01_staticComponent.js
+++ components/exercises/01_staticComponent.js
@@ -3,7 +3,26 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
-      <h1>TODO</h1>
+    <div>
+      <h1>Enterprise Software Systems - Modules table</h1>
+      <table class="table table-bordered">
+        <tr>
+          <th>Name</th>
+          <th>No lectures</th>
+          <th>No practicals</th>
+        </tr>
+        <tr>
+          <th>Enterprise Software Systems</th>
+          <th>2</th>
+          <th>2</th>
+        </tr>
+        <tr>
+          <th>Data Mining</th>
+          <th>2</th>
+          <th>2</th>
+        </tr>
+      </table>
+    </div>
   );
 };
 
