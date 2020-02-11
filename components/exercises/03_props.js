703ccad5325ad6e16a85a2bf1fc53a0518c6f31e
Bartosz Osowski <bartosz.osowski@errigal.com>
2020-02-11 23:29:18 +0000

    Exercise 3 completed.

--- components/exercises/03_props.js
+++ components/exercises/03_props.js
@@ -2,9 +2,40 @@ import React from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
-const Demo = () => {
+const Demo = (props) => {
+  var items = props.modules.map((item, key) =>
+    <tr>
+      <th>{item.name}</th>
+      <th>{item.noLectures}</th>
+      <th>{item.noPracticals}</th>
+    </tr>
+  );
+
   return (
-      <h1>TODO</h1>
+    <div>
+      <h1>{props.course}</h1>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>{props.modules[0].name}</th>
+            <th>{props.modules[0].noLectures}</th>
+            <th>{props.modules[0].noPracticals}</th>
+          </tr>
+          <tr>
+            <th>{props.modules[1].name}</th>
+            <th>{props.modules[1].noLectures}</th>
+            <th>{props.modules[1].noPracticals}</th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
