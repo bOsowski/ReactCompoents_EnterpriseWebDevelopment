eaceada665cafd1a093eef18b1e0710ca1f683f4
Bartosz Osowski <bartosz.osowski@errigal.com>
2020-02-11 23:43:03 +0000

    Exercise 4 completed.

--- components/exercises/04_iteration.js
+++ components/exercises/04_iteration.js
@@ -2,9 +2,31 @@ import React from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
-const Demo = () => {
+const Demo = (props) => {
+  const items = props.modules.map((item, key) =>
+    <tr>
+      <th>{item.name}</th>
+      <th>{item.noLectures}</th>
+      <th>{item.noPracticals}</th>
+    </tr>
+  );
+
   return (
-      <h1>TODO</h1>
+    <div>
+      <h1>{props.course}</h1>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
